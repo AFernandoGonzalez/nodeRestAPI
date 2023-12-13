2aa1e275b84f4851237b5b576423771df3bbe0cb
Fernando <ferg.developer@gmail.com>
2023-12-13 11:09:20 -0500

    added api limiter for limmiting api request in a set amout of time per hours per IP

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-
+const rateLimit = require("express-rate-limit");
 const bookRoute = require("./src/routes/bookRoute");
 const connectDB = require("./src/db/db");
 
 dotenv.config();
 
 const app = express();
+connectDB();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-connectDB();
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again in 15 minutes!"
+});
 
 const PORT = process.env.PORT || 8000;
 
@@ -19,7 +24,7 @@ app.get("/", (req, res) => {
   res.send("My Rest API");
 });
 
-app.use("/api", bookRoute);
+app.use("/api", apiLimiter, bookRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
